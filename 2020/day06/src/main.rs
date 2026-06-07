bdb06eae8d241ff87fdb8f5ba51901d8644900a7
Terje Wiig Mathisen <terje.mathisen@cognite.com>
2026-06-07 09:23:24 +0200

    Skip main loop exit test

--- 2020/day06/src/main.rs
+++ 2020/day06/src/main.rs
@@ -1,5 +1,5 @@
 // Fastest run (Surface): 15.5 us
-//              Acer: 4.4 us        vs 38 us (7.9 us next day!?!)
+//              Acer: 5.0 us        vs 38 us
 use std::fs;
 use devtimer::DevTime;
 use devtimer::run_benchmark;
@@ -14,18 +14,19 @@ fn process(inp:&str) -> (usize, usize)
     let mut any:u32 = 0;
     let mut curr = 0;
     let len = bytes.len();
-    while i < len {
+    loop {
         let b = bytes[i]; i += 1;
         if b == b'\n' {
             all &= curr;
             any |= curr;
             curr = 0;
-            if bytes[i] == b'\n' {
-                i += 1;
+            if bytes[i] == b'\n' { // Is this a double newline ending a section? Safe look-ahead due to input padding!
                 part1 += any.count_ones() as usize;
                 part2 += all.count_ones() as usize;
                 all = u32::MAX;
                 any = 0;
+                i += 1;
+                if i >= len {break} // The input can only end here, so the main loop does not have to test
             }
 
         } else { //if b >= b'a' {
