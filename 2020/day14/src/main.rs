c754545866ccd042f502db14f75a27c6457f6cd5
Terje Wiig Mathisen <terje.mathisen@cognite.com>
2026-06-13 20:48:29 +0200

    day14 _pdep_u64 intrinsic

--- 2020/day14/src/main.rs
+++ 2020/day14/src/main.rs
@@ -1,59 +1,146 @@
 // Fastest run (Surface):
-//              Acer:     1.077 us
+//              Acer:    8.658 us for part1 
+//                       4.727 ms both parts, sw pdep 
+//                       1.967 ms, _pdep_u64 for floating bits distribution
 use std::fs;
 use devtimer::DevTime;
 use devtimer::run_benchmark;
+use rustc_hash::FxHashMap;
 
-fn process(inp:&str) -> (usize, usize)
+#[cfg(target_arch = "x86_64")]
+use core::arch::x86_64::_pdep_u64;
+
+fn _pext(n:u64, mask:u64) -> u64
+{
+    let mut ubit = 1;
+    let mut res = 0;
+    for i in 0..64 {
+        let ibit = 1 << i;
+        if (mask & ibit) != 0 {
+            if n & ibit != 0 { res |= ubit;}
+            ubit <<= 1;
+        }
+    }
+    res
+}
+
+fn _pdep(n:u64, mask:u64) -> u64
+{
+    let mut ubit = 1;
+    let mut res = 0;
+    for i in 0..64 {
+        let ibit = 1 << i;
+        if mask & ibit != 0 {
+            if n & ubit != 0 {
+                res |= ibit;
+            }
+            ubit <<= 1;
+        }
+    }
+    res
+}
+
+struct Mask {
+    zero:u64,
+    one:u64,
+    x:u64,
+}
+
+fn parsemask(inp:&[u8]) -> (usize, Mask)
+{
+    let mut mask = Mask{zero:0,one:0,x:0};
+    let mut retpos = 0;
+    for i in 7.. {
+        if inp[i] == b'\n' {retpos = i+1; break}
+        mask.zero <<= 1;
+        mask.one <<= 1;
+        mask.x <<= 1;
+        match inp[i] {
+            b'0' => mask.zero |= 1,
+            b'1' => mask.one |= 1,
+            b'X' => mask.x |= 1,
+            _ => panic!("Bad input mask"),
+        }
+    }
+    (retpos, mask)
+}
+
+fn parsemem(inp:&[u8]) -> (usize, u64, u64)
+{
+    let mut i = 0;
+    let mut adr = 0;
+    i += 4;
+    while inp[i] != b']' {
+        adr = adr*10 + (inp[i] - b'0') as u64;
+        i += 1;
+    }
+    i += 4;
+    let mut val = 0;
+    while inp[i] != b'\n' {
+        val = val*10 + (inp[i] - b'0') as u64;
+        i += 1;
+    }
+    (i+1, adr, val)
+}
+
+fn process(inp:&str) -> (u64, u64)
 {
     let bytes = inp.as_bytes();
+    let mut mask = Mask{zero:0, one:0, x:0};
+    let mut adr;
+    let mut val;
     let mut i = 0;
-    let mut buses:Vec<usize> = Vec::new();
-
-    let mut start:usize = 0;
-    while bytes[i] != b'\n' {
-        start = start*10 + (bytes[i] - b'0') as usize;
-        i += 1;
-    }
-    let mut earliest = usize::MAX;
-    let mut part1 = 0;
+    let mut di;
+    let mut map: FxHashMap<u64, u64> = FxHashMap::default();
+    let mut map2: FxHashMap<u64, u64> = FxHashMap::default();
+    let mut perm = 0;
     while i < bytes.len() {
-        let b = bytes[i]; i += 1;
-        if b == b'x' {
-            buses.push(usize::MAX);
+        if bytes[i+1] == b'a' {
+            // mask = 0X11XX1X010X01101000X01X011101100000
+            (di, mask) = parsemask(&bytes[i..]);
+            i += di;
+            perm = 1 << mask.x.count_ones();
+//            println!("zero:{:b}, one:{:b}, x:{:b}", mask.zero, mask.one, mask.x);
         }
-        else {
-            let mut n = (b - b'0') as usize;
-            while bytes[i] != b',' {
-                n = n*10 + (bytes[i] - b'0') as usize; i += 1;
-            }
-            buses.push(n);
-            let loops = start/n;
-            let next = (loops+1)*n;
-            let wait = next-start;
-            if wait < earliest {
-                earliest = wait;
-                part1 = wait * n;
-            }
-        }
-        i += 1;
-    }
+        else if bytes[i+1] == b'e' {
+            // mem[4634] = 907
+            (di, adr, val) = parsemem(&bytes[i..]);
+            i += di;
+//            println!("adr = {adr:b}, val = {val:b}");
+            let val1 = (val | mask.one) & !mask.zero;
+//            println!("masked value = {val:b}");
+            map.insert(adr, val1);
 
-    let mut prod = buses[0];
-    let mut part2 = prod;
-    for idx in 1..buses.len() {
-        let id = buses[idx];
-        if id == usize::MAX {continue}
-        //println!("product = {prod}");
-        while ((part2 + idx) % id) != 0 { 
-            part2 += prod;
+            // part2
+            adr &= mask.zero;
+            adr |= mask.one;
+//            println!("adr = {adr:b}, perm: {perm}");
+            for i in 0..perm {
+//                let floating = pdep(i, mask.x);
+                let floating = unsafe { _pdep_u64(i as u64, mask.x as u64) } as u64;
+                let a = adr | floating;
+//                println!("floating addr: {a:b}");
+                //if map2.contains_key(&a) { panic!("Duplicate address {a}")}
+                map2.insert(a, val);
+            }
         }
-        prod *= id;
+        else {panic!("Bad input at offset {i}");}
     }
+//    println!("part2 permutations: {perm}");
+    let mut sum = 0;
+    for (_key, val) in &map {
+        sum += val;
+    }
+    let part1 = sum;
+    sum = 0;
+    for (_key, val) in &map2 {
+        sum += val;
+    }
+    let part2 = sum;
     (part1,part2)
 }
 
-fn _process_1000(inp:&str) -> (usize, usize)
+fn _process_1000(inp:&str) -> (u64, u64)
 {
     for _ in 0..1000 {
         process(&inp);
@@ -65,12 +152,11 @@ fn main() {
     let fname = "input.txt"; // instead of args[1]
 //    let fname = "test.txt"; // instead of args[1]
     let mut input = fs::read_to_string(fname).expect("Error reading input file");
-    if input.as_bytes()[input.len()-1] == b'\n' {input.pop();}
-    if input.as_bytes()[input.len()-1] != b',' {input.push(',');}
+    if input.as_bytes()[input.len()-1] != b'\n' {input.push('\n');}
 
     let mut devtime = DevTime::new_simple();
 
-    let bench_result = run_benchmark(1000, |_| { _process_1000(&input); }); bench_result.print_stats();
+    let bench_result = run_benchmark(100, |_| { process(&input); }); bench_result.print_stats();
 
     //process(&input);
     devtime.start();
