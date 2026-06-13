7416453508eb38db6f5d8cc30a4e640b99b8a5a5
Terje Wiig Mathisen <terje.mathisen@cognite.com>
2026-06-13 11:36:27 +0200

    Skip the x'es

--- 2020/day13/src/main.rs
+++ 2020/day13/src/main.rs
@@ -1,40 +1,35 @@
 // Fastest run Surface: 1.457 us
-//                Acer: 1.067 us
+//                Acer: 0.820 us
 use std::fs;
 use devtimer::DevTime;
 use devtimer::run_benchmark;
-//use std::iter;
-
 struct Bus {
     id:usize,
+    idx:usize,
 }
 
 fn process(inp:&str) -> (usize, usize)
 {
     let bytes = inp.as_bytes();
-    let mut i = 0;
     let mut buses:Vec<Bus> = Vec::new();
-
+    let mut i = 0;
     let mut start:usize = 0;
     while bytes[i] != b'\n' {
         start = start*10 + (bytes[i] - b'0') as usize;
         i += 1;
     }
-    //print!("start={start}");
-    i += 1;
+    i += 1; // Skip to the second line
     let mut earliest = usize::MAX;
     let mut part1 = 0;
+    let mut idx = 0;
     while i < bytes.len() {
         let b = bytes[i]; i += 1;
-        if b == b'x' {
-            buses.push(Bus{id:usize::MAX});
-        }
-        else {
+        if b != b'x' {
             let mut n = (b - b'0') as usize;
             while bytes[i] != b',' {
                 n = n*10 + (bytes[i] - b'0') as usize; i += 1;
             }
-            buses.push(Bus{id:n});
+            buses.push(Bus{id:n, idx:idx});
             //print!(",{n}");
             let loops = start/n;
             let next = (loops+1)*n;
@@ -45,15 +40,16 @@ fn process(inp:&str) -> (usize, usize)
             }
         }
         i += 1;
+        idx += 1;
     }
     //println!();
 
     let mut prod = buses[0].id;
     let mut part2 = prod;
     //println!("Bus {prod} leaves at {part2}+0");
-    for idx in 1..buses.len() {
-        let id = buses[idx].id;
-        if id == usize::MAX {continue}
+    for i in 1..buses.len() {
+        let id = buses[i].id;
+        let idx = buses[i].idx;
         //println!("product = {prod}");
         while ((part2 + idx) % id) != 0 { 
             part2 += prod;
