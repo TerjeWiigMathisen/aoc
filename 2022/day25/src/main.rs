a10020143b811f80dea4869166c9fee4d29b4476
Terje Mathisen <terje.mathisen@tmsw.no>
2026-08-25 19:40:51 +0200

    256-byte table

--- 2022/day25/src/main.rs
+++ 2022/day25/src/main.rs
@@ -1,6 +1,6 @@
 //aoc 2022 day25
-// Fastest run: Surface Pro 8 754 ns
-//                       Acer
+// Fastest run: Surface Pro 8 1456 ns
+//                       Acer  651 ns
 
 //use devtimer::DevTime;
 use devtimer::run_benchmark;
@@ -10,21 +10,20 @@ const BASE:i64 = 5;
 const VALUE:[i8;256] = [
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,1,2,0,0,0,0,0,0,0,0,0,0,-2,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 const DIGITS:[u8;5] = [b'0',b'1',b'2',b'=',b'-'];
 
-fn frombase(digits:&[u8]) -> i64
+fn _frombase(digits:&[u8]) -> i64
 {
 	let mut n:i64 = 0;
 	for i in 0..digits.len() {
 		n = n*BASE + (VALUE[digits[i] as usize] as i64);
-//        println!("n={n} <- {} <- {}", VALUE[digits[i] as usize - 45], digits[i]);
 	}
 	n
 }
@@ -37,7 +36,7 @@ fn tobase(n:i64) -> String
 	loop {
 		let r = n % BASE; // Always positive remainder for positive $BASE
 		let d = DIGITS[r as usize];
-		n = (n-VALUE[d as usize - 45]) / BASE; // Always exact division!
+		n = (n-VALUE[d as usize] as i64) / BASE; // Always exact division!
         p -= 1;
         buf[p] = d;
         if n == 0 {break;}
@@ -45,31 +44,31 @@ fn tobase(n:i64) -> String
 	std::str::from_utf8(&buf[p..buf.len()]).unwrap().to_string()
 }
 
-
 #[inline(never)]
 pub fn process(inp:&[u8]) -> (i64,String)
 {
     let mut part1 = 0;
-    let mut value:[i8;256] = [0;256];
-    value[b'-' as usize] = -1;
-    value[b'1' as usize] = 1;
-    value[b'2' as usize] = 2;
-    value[b'=' as usize] = -2;
     let mut n = 0;
-    for j in 0..1000 {
     for i in 0..inp.len() {
         if inp[i] == b'\n' {
             part1 += n;
             n = 0;
             continue;
         }
-//		n = n*BASE + (VALUE[inp[i] as usize - 45]);
-		n = n*BASE + (value[inp[i] as usize] as i64);
+		n = n*BASE + (VALUE[inp[i] as usize] as i64);
     }
     part1 += n;
-    }
-    part1 /= 1000;
     (part1,tobase(part1))
+}
+
+pub fn p1k(inp:&[u8]) -> (i64,String)
+{
+    let mut p1 = 0;
+    let mut p2 = "".to_string();
+    for _ in 0..1000 {
+        (p1,p2) = process(inp);
+    }
+    (p1,p2)
 }
 
 fn main() {
